d2a4a3b64fa9866b8b436a298df82b1872545597
Kevin <kevinalexanderduran8@gmail.com>
2022-04-22 16:16:15 -0700

    CART ITEMS:
    -cart items given complete css makeover: FINISHED

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -3,7 +3,7 @@ import './Cart.css'
 import Apu from '../assets/apu.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowTurnRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowTurnRight, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
 import{CartToggleContext} from '../Helper/Context'
 
@@ -24,17 +24,42 @@ function Cart() {
             <div className='cart--open--button--checkout__container'>
                 <button className='cart--open--button--checkout'>
                 <span className="cart--open--button--checkout--i"><FontAwesomeIcon icon={faArrowTurnRight}  /></span>
-                <p className="cart--open--button--checkout--p">go to cart</p>
+                <p className="cart--open--button--checkout--p">go to checkout</p>
                 </button>
             </div>
         </div>
         <div className='cart--open--bottom'>
+            {/* below should be a component */}
+
             <div className='cart--open--bottom__item'>   
                 <div className='cart--open__item--image__container'>
                     <img className='cart--open__item--image' src={Apu}/>
                 </div>
-                <div>
-
+                <div className='cart--open__item--info__container'>
+                    <div className='cart__item--info--top'>
+                        <div className='cart__item--info--top--left'>
+                            <p>Weihenstephan Hefe-Weiss 6 Pack 12oz Bottles</p>
+                        </div>
+                        <div className='cart__item--info--top--right'>
+                            <button className='cart__item--close__button'>
+                                <span className="cart__item--close__button--icon"><FontAwesomeIcon icon={faTrashCan}  /></span>
+                            </button>
+                        </div>
+                    </div>
+                    <div className='cart__item--info--bottom'>
+                        <div className='cart__item--quantity__container'>
+                            <div className="cart__item--quantity--button__container">
+                                <button className='cart__item--quantity--button'>-</button>
+                                <p className='cart__item--quantity--total'>0</p>
+                                <button className='cart__item--quantity--button'>+</button>                          
+                            </div>
+                           </div>
+                        <div className='cart__item--total__container'>
+                            <div className='cart__item--total'>
+                                <p className='cart__item--total--number'>$69</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>      
             </div>
         </div>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -8,8 +8,8 @@ function Header() {
     <div className="header__container">
         <div className='header__container--text--container'>
             <div className='header__container--text'>
-                <h1>Convenience forever</h1>
-                <h1>Freshness <span className='header__container--text--red'>never</span></h1>
+                <h1>Convenience forever.</h1>
+                <h1>Freshness <span className='header__container--text--red'>never.</span></h1>
             </div>
         </div>
         <div className='header__container--img'>
