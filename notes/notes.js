aaa1fa20e4d35bcabcf67b35b7fef780b33291f8
Kevin <kevinalexanderduran8@gmail.com>
2022-04-23 18:28:47 -0700

    APP:
    -began notes.js for small todo items list

---
+++ notes/notes.js
@@ -0,0 +1,23 @@
+/* 
+
+
+    TODO:
+        -CART ITEM: quantity not working
+        -NAV: search input not working
+        -PRODUCTS: pagination, map limit, not working
+        -FOOTER: not built
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/ 
--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -33,70 +33,50 @@ function Cart() {
         <div className='cart--open--bottom'>
             {/* below should be a component */}
             
+            {cartItemData.map((data)=>{
 
-
-
-
-
-
-
-
-
-
-    
-
-
-
-    {cartItemData.map((data)=>{
-
-        return (
-            <div className='cart--open--bottom__item'>   
-                    <div className='cart--open__item--image__container'>
-                        <img className='cart--open__item--image' src={data.imageLink}/>
+                return (
+                    <div className='cart--open--bottom__item'>   
+                            <div className='cart--open__item--image__container'>
+                                <img className='cart--open__item--image' src={data.imageLink}/>
+                            </div>
+                            <div className='cart--open__item--info__container'>
+                                <div className='cart__item--info--top'>
+                                    <div className='cart__item--info--top--left'>
+                                        <p>{data.itemName}</p>
+                                    </div>
+                                    <div className='cart__item--info--top--right'>
+                                        <button 
+                                            onClick={()=>{
+                                                let newList = cartItemData.filter((item)=>{
+                                                        return (item.itemName != data.itemName)
+                                                })
+                                                setCartItemData(newList)
+                                                console.log(newList)
+                                            }} 
+                                            className='cart__item--close__button'>
+                                            <span className="cart__item--close__button--icon"><FontAwesomeIcon icon={faTrashCan}  /></span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className='cart__item--info--bottom'>
+                                    <div className='cart__item--quantity__container'>
+                                        <div className="cart__item--quantity--button__container">
+                                            <button onClick={()=> setItemQuantity(itemQuantity-1)} className='cart__item--quantity--button'>-</button>
+                                            <p className='cart__item--quantity--total'>{itemQuantity}</p>
+                                            <button onClick={()=> setItemQuantity(itemQuantity+1)} className='cart__item--quantity--button'>+</button>                          
+                                        </div>
+                                    </div>
+                                    <div className='cart__item--total__container'>
+                                        <div className='cart__item--total'>
+                                            <p className='cart__item--total--number'>${(data.price * itemQuantity).toFixed(2)}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>  
                     </div>
-                    <div className='cart--open__item--info__container'>
-                        <div className='cart__item--info--top'>
-                            <div className='cart__item--info--top--left'>
-                                <p>{data.itemName}</p>
-                            </div>
-                            <div className='cart__item--info--top--right'>
-                                <button 
-                                    onClick={()=>{
-                                        let newList = cartItemData.filter((item)=>{
-                                                return (item.itemName != data.itemName)
-                                        })
-                                        setCartItemData(newList)
-                                        console.log(newList)
-                                    }} 
-                                    className='cart__item--close__button'>
-                                    <span className="cart__item--close__button--icon"><FontAwesomeIcon icon={faTrashCan}  /></span>
-                                </button>
-                            </div>
-                        </div>
-                        <div className='cart__item--info--bottom'>
-                            <div className='cart__item--quantity__container'>
-                                <div className="cart__item--quantity--button__container">
-                                    <button onClick={()=> setItemQuantity(itemQuantity-1)} className='cart__item--quantity--button'>-</button>
-                                    <p className='cart__item--quantity--total'>{itemQuantity}</p>
-                                    <button onClick={()=> setItemQuantity(itemQuantity+1)} className='cart__item--quantity--button'>+</button>                          
-                                </div>
-                            </div>
-                            <div className='cart__item--total__container'>
-                                <div className='cart__item--total'>
-                                    <p className='cart__item--total--number'>${(data.price * itemQuantity).toFixed(2)}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>  
-            </div>
-        )
-
-
-
-
-    })}
-
-
+                )
+            })}
         </div>
         
     </div>
