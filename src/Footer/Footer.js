38231fc913ab0ace36c721cfe2f2b0641e3fb6b6
Kevin <kevinalexanderduran8@gmail.com>
2022-04-26 19:35:27 -0700

    FOOTER:
    -footer is now completely responsive

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -15,8 +15,12 @@ function Footer() {
         <div className='footer--top'>
             <div className='footer--top--one'>
                 <div className='footer--top--word__container'>
-                    <h5 className='footer--top--word__container--top'>* we only accept bitcoin</h5>
-                    <h2 className='footer--top--word__container--bottom'>Thank you, come again.</h2>
+                    <div className='footer--top--word__container--small'>
+                        <h5 className='footer--top--word__container--top'>* we only accept bitcoin</h5>
+                    </div>
+                    <div className='footer--top--word__container--small'>
+                        <h2 className='footer--top--word__container--bottom'>Thank you, come again.</h2>
+                    </div>         
                 </div>
                 <div className='footer--top--input__container'>
                     <input className='footer--top--input' placeholder='I will ignore your email.'></input>
@@ -32,28 +36,25 @@ function Footer() {
         </div>
         <div className='footer--bottom'>
             <div className='footer--bottom--one'>
-                <div className='footer--bottom--one--left'>
+                <div className='footer--bottom--one--logo footer--bottom--one--item'>
                     <h2 className='footer--logo--name'>kwik<span className='greenLine'>-</span>e<span className='greenLine'>-</span>mart</h2>
                 </div>
-                <div >
-                    <ul>
-                        <li className='footer--bottom--li green'>About</li>
-                        <li className='footer--bottom--li green'>Jobs</li>
-                        <li className='footer--bottom--li green'>Docs</li>
-                    </ul>
-
+                <div className='footer--bottom--one--item' >
+                        <p className='footer--bottom--list'>About</p>
+                        <p className='footer--bottom--list'>Jobs</p>
+                        <p className='footer--bottom--list'>Docs</p>
                 </div>
             </div>
             <div className='footer--bottom--two'>
-                <div>
-                        <li className='footer--bottom--li green'>Terms and Conditions</li>
-                        <li className='footer--bottom--li green'>Privacy Policy</li>
-                        <li className='footer--bottom--li green'>Cookie Policy</li>
+                <div className='footer--bottom--two--item'>
+                        <p className='footer--bottom--list green'>Terms and Conditions</p>
+                        <p className='footer--bottom--list green'>Privacy Policy</p>
+                        <p className='footer--bottom--list green'>Cookie Policy</p>
                 </div>
-                <div>
-                    <h4 className='footer--bottom--li'>Let's Chat</h4>
-                    <p className='footer--bottom--li'>kwikemart@gmail.com</p>
-                    <div className='footer--bottom--li'>
+                <div className='footer--bottom--two--item'>
+                    <h4 className='footer--bottom--list'>Let's Chat</h4>
+                    <p className='footer--bottom--list'>kwikemart@gmail.com</p>
+                    <div className='footer--bottom--list'>
                         <span className='footer--social--link'><FaTwitter/></span>
                         <span className='footer--social--link'><FaInstagram/></span>
                         <span className='footer--social--link'><FaEnvelope/></span>
