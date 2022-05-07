8aa79abedfb35c683682507ecf6e978916be0cc7
Kevin <kevinalexanderduran8@gmail.com>
2022-05-07 04:51:38 -0700

    HEADER:
    -header font and display changed for better look

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -8,8 +8,10 @@ function Header() {
     <div className="header__container">
         <div className='header__container--text--container'>
             <div className='header__container--text'>
-                <h1>Convenience forever.</h1>
-                <h1>Freshness <span className='header__container--text--red'>never.</span></h1>
+                <h1>Convenience</h1>
+                <h1>forever.</h1>
+                <h1>Freshness</h1>
+                <h1><span className='header__container--text--red'>never.</span></h1>
             </div>
         </div>
         <div className='header__container--img'>
