94a5139c68915a60f48612aae06d26c8ffda2dd7
Kevin <kevinalexanderduran8@gmail.com>
2022-05-07 20:12:27 -0700

    NOTES:
    -new notes added

--- notes/notes.js
+++ notes/notes.js
@@ -2,12 +2,13 @@
 
 
     TODO:
+        -NAVIGATION: input should include button
         -CART ITEM: quantity not working
         -CART ITEM: duplicates must be removed
         -PRODUCTS: pagination, map limit, not working
+        -PRODUCTS: num of items info popup
         -CART: go to checkout not working
         -CART: total should be added on cart
         -CART: local storage items in cart
-    BRAINSTORM:
-        -
+
 */ 
